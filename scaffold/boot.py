272b64dee892ad4c620cc0255cf480a4434efd43
Adam Alton <adamalton@gmail.com>
2016-03-24 11:25:35 +0000

    Making get-or-creation of the Config object transactional.

--- scaffold/boot.py
+++ scaffold/boot.py
@@ -5,13 +5,13 @@ PROJECT_DIR = dirname(dirname(abspath(__file__)))
 SITEPACKAGES_DIR = join(PROJECT_DIR, "sitepackages")
 APPENGINE_DIR = join(SITEPACKAGES_DIR, "google_appengine")
 
+
 def fix_path():
     if exists(APPENGINE_DIR) and APPENGINE_DIR not in sys.path:
         sys.path.insert(1, APPENGINE_DIR)
 
     if SITEPACKAGES_DIR not in sys.path:
         sys.path.insert(1, SITEPACKAGES_DIR)
-
 
 
 def get_app_config():
@@ -23,14 +23,18 @@ def get_app_config():
         """A simple key-value store for application configuration settings."""
         secret_key = ndb.StringProperty()
 
-    # Create a random SECRET_KEY hash
+    # Create a random SECRET_KEY
     chars = 'abcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*(-_=+)'
-    secret_key = get_random_string(50, chars)
 
-    key = ndb.Key(Config, 'config')
-    entity = key.get()
-    if not entity:
-        entity = Config(key=key)
-        entity.secret_key = str(secret_key)
-        entity.put()
-    return entity
+    @ndb.transactional()
+    def txn():
+        # Get or create the Config in a transaction, so that if it doesn't exist we don't get 2
+        # threads creating a Config object and one overwriting the other
+        key = ndb.Key(Config, 'config')
+        entity = key.get()
+        if not entity:
+            entity = Config(key=key)
+            entity.secret_key = get_random_string(50, chars)
+            entity.put()
+        return entity
+    return txn()
