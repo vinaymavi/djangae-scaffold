e89b4d25c1bb7864b1caead3ca13723a7ae37053
David Buxton <david@gasmark6.com>
2016-05-31 11:47:28 +0100

    Make the scaffold rename script replace dash with underscore.
    Python names can't use dashes. If you rename the project to "foo-bar"
    then the main package will be named "foo_bar".

--- rename_scaffold_app.sh
+++ rename_scaffold_app.sh
@@ -4,7 +4,10 @@ if [ "$#" -ne 1 ]; then
     exit 1
 fi
 
-perl -i -pe "s/scaffold/$1/g;" app.yaml manage.py
-perl -i -pe "s/scaffold/$1/g;" scaffold/*.py
+# Make a Python-friendly version of the project name.
+PY_PROJECT_NAME=$(echo $1 | tr '-' '_')
 
-mv ./scaffold "$1"
+perl -i -pe "s/scaffold/$1/g;" app.yaml
+perl -i -pe "s/scaffold/$PY_PROJECT_NAME/g;" manage.py scaffold/*.py
+
+mv ./scaffold "$PY_PROJECT_NAME"
