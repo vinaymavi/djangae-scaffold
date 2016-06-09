c299a8f4dc87e74b9b4601bd1c8a0f40868f14e3
Danny Milsom <danny@potatolondon.com>
2016-06-09 12:41:03 +0100

    Use `is_production_environment()` not `on_production()`
    * on_production() has depreciation warnings in the latest Djangae release.

--- scaffold/wsgi.py
+++ scaffold/wsgi.py
@@ -12,10 +12,10 @@ fix_path()
 
 import os
 from django.core.wsgi import get_wsgi_application
+from djangae.environment import is_production_environment
 from djangae.wsgi import DjangaeApplication
-from djangae.utils import on_production
 
-settings = "scaffold.settings_live" if on_production() else "scaffold.settings"
+settings = "scaffold.settings_live" if is_production_environment() else "scaffold.settings"
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", settings)
 
 
