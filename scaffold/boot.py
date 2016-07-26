fb7a126e73b98948d35273a6fb84ada2a42e433c
Adam Alton <adamalton@gmail.com>
2016-07-26 11:39:17 +0100

    Add a system check to ensure that Django templates use the cached loader on production.

--- scaffold/boot.py
+++ scaffold/boot.py
@@ -46,3 +46,4 @@ def register_custom_checks():
     register(checks.check_csp_sources_not_unsafe, Tags.security, deploy=True)
     register(checks.check_session_csrf_enabled, Tags.security)
     register(checks.check_csp_is_not_report_only, Tags.security)
+    register(checks.check_cached_template_loader_used, Tags.caches, deploy=True)
--- scaffold/checks.py
+++ scaffold/checks.py
@@ -34,6 +34,7 @@ CSP_SOURCE_NAMES = [
     'CSP_CONNECT_SRC',
 ]
 
+
 def check_csp_sources_not_unsafe(app_configs, **kwargs):
     errors = []
     for csp_src_name in CSP_SOURCE_NAMES:
@@ -44,3 +45,19 @@ def check_csp_sources_not_unsafe(app_configs, **kwargs):
                 hint="Please remove 'unsafe-inline'/'unsafe-eval' from your CSP policies",
             ))
     return errors
+
+
+def check_cached_template_loader_used(app_configs, **kwargs):
+    """ Ensure that the cached template loader is used for Django's template system. """
+    for template in settings.TEMPLATES:
+        if template['BACKEND'] != "django.template.backends.django.DjangoTemplates":
+            continue
+        loaders = template['OPTIONS'].get('loaders', [])
+        for loader_tuple in loaders:
+            if loader_tuple[0] == 'django.template.loaders.cached.Loader':
+                return []
+        error = Error(
+            "CACHED_TEMPLATE_LOADER_NOT_USED",
+            hint="Please use 'django.template.loaders.cached.Loader' for Django templates"
+        )
+        return [error]
