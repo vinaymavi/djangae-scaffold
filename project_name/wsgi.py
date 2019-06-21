1254bbbe25f764bf0165773a12b1b7c045df9a7a
Adam Alton <adamalton@gmail.com>
2019-06-21 14:46:56 +0100

    Fix wsgi.py to add dev libs to fix path for local server

--- project_name/wsgi.py
+++ project_name/wsgi.py
@@ -7,10 +7,15 @@ For more information on this file, see
 https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
 """
 
-from {{ project_name }}.boot import fix_path
-fix_path()
-
 import os
+from {{ project_name }}.boot import fix_path
+
+# This is a copy from djangae.environment, but we can't import that until we've called fix_path()
+def is_development_environment():
+    return 'SERVER_SOFTWARE' not in os.environ or os.environ['SERVER_SOFTWARE'].startswith("Development")
+
+fix_path(include_dev_libs_path=is_development_environment())
+
 from django.core.wsgi import get_wsgi_application
 from djangae.environment import is_production_environment
 from djangae.wsgi import DjangaeApplication
